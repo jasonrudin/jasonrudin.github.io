59beed4f91721ed8ae8352450d5cf5a3738586d3
Jason Rudin <jasonrudin@Jasons-MacBook-Pro.local>
2017-05-04 21:57:35 -0400

    First commit, all files

---
+++ js/script.js
@@ -0,0 +1,411 @@
+var dummy = {
+	'title' : 'About me',
+	'date' : 'January 1992',
+	'thumbnail_link' : 'img/headshot2.png',
+	'main_content_link' : '<img class = "display_img" src =img/headshot2.png width = 100% style = "max-width: 450px"/>',
+	'description' : "<div class = \"main_content\"><h1>Look ma, I made a website!</h1> <h3>Bio:</h3><ul><li>Currently, Sr. Business Analyst @ McKinsey</li><li>Former Associate Product Marketing Manager Intern @ Google (where I did some Product Management too)</li><li>2014 Grad from UPenn Management and Technology Program; degrees in Engineering (Digital Media Design - CS + Art) and Business (Management and Finance).</li></ul><h3>About me:</h3><ul><li>Continuously learning</li><li>!</li><li>Passionate about art, tech, organizational behvaior, and [country] music</li><li>Thinking about energy/sustainability, <a target='_blank' href = 'https://medium.com/@jasonrudin/exploring-education-a05b7a68edb8'>education</a>, and what makes cities work</li><li>Re-teaching myself how to play the piano; neighbors aren't thrilled.</li></ul><h3>Let's talk:</h3><span>j.b.rudin[at]gmail.com</span></div>",
+	'categorization' : 'art',
+	'medium' : '',
+	'content_type' : 'video'
+};
+
+var dummy4 = {
+	'title' : 'Minimalist Pixar movie posters',
+	'date' : 'August 2015',
+	'thumbnail_link' : 'img/pixar_thumbnail.png',
+	'main_content_link' : '<a class = "fancybox" data-fancybox-group = "pixar" href ="img/monsters_poster.png"> <img class = "display_img multi_display" src ="img/monsters_poster.png" width = 250px/></a><a class = "fancybox" data-fancybox-group = "pixar" href ="img/nemo_poster.png"><img class = "display_img multi_display" src ="img/nemo_poster.png" width = 250px/></a><a class = "fancybox" data-fancybox-group = "pixar" href ="img/up_poster.png"><img class = "display_img multi_display" src ="img/up_poster.png" width = 250px/></a>',
+	'description' : '<div class = \"main_content\"><h1>Minimalist Pixar movie posters</h1><h2>August 2015</h2><p>Series of three posters, inspired by some of my favorite Pixar movies. Designed in Adobe Illustrator.</p></div>',
+	'categorization' : 'firstART',
+	'medium' : 'digital',
+	'content_type' : 'video'
+};
+
+var dummy6 = {
+	'title' : 'Flowers',
+	'date' : 'April 2014',
+	'thumbnail_link' : 'img/flowers_thumbnail.png',
+	'main_content_link' : '<a class = "fancybox" data-fancybox-group = "flowers" href ="img/flowers.png"> <img class = "display_img multi_display" src ="img/flowers.png" width = 520px/></a>',
+	'description' : '<div class = \"main_content\"><h1>Flowers</h1><h2>April 2014</h2><h2>Oil, 11" x 14"</h2></div>',
+	'categorization' : 'art',
+	'medium' : 'oil',
+	'content_type' : 'video'
+};
+
+var dummy7 = {
+	'title' : 'Baseball and glove',
+	'date' : 'January 2015',
+	'thumbnail_link' : 'img/ball_and_glove_thumbnail.png',
+	'main_content_link' : '<a class = "fancybox" data-fancybox-group = "baseball" href ="img/ball_and_glove.png"> <img class = "display_img multi_display" src ="img/ball_and_glove.png" width = 700px/></a>',
+	'description' : '<div class = \"main_content\"><h1>Baseball and glove</h1><h2>January 2015</h2><h2>Colored pencil, 10" x 8"</h2></div>',
+	'categorization' : 'art',
+	'medium' : 'colored pencil',
+	'content_type' : 'video'
+};
+
+var blocks = {
+	'title' : 'Blocks',
+	'date' : 'May 2014',
+	'thumbnail_link' : 'img/abstract_blocks_thumbnail.png',
+	'main_content_link' : '<a class = "fancybox" data-fancybox-group = "block" href ="img/abstract_blocks.png"> <img class = "display_img multi_display" src ="img/abstract_blocks.png" width = 900px/></a>',
+	'description' : '<div class = \"main_content\"><h1>Blocks</h1><h2>May 2014</h2><h2>Oil, 16" x 32"</h2></div>',
+	'categorization' : 'art',
+	'medium' : 'oil',
+	'content_type' : 'video'
+};
+
+var dummy8 = {
+	'title' : '[Untitled]',
+	'date' : 'December 2009',
+	'thumbnail_link' : 'img/strawberries_thumb.png',
+	'main_content_link' : '<a class = "fancybox" data-fancybox-group = "berries" href ="img/A Sweet Pair.png"> <img class = "display_img multi_display" src ="img/A Sweet Pair.png" width = 800px/></a>',
+	'description' : '<div class = \"main_content\"><h1>[Untitled]</h1><h2>December 2009</h2><h2>Colored pencil, 8" x 10"</h2></div>',
+	'categorization' : 'art',
+	'medium' : 'colored pencil',
+	'content_type' : 'video'
+};
+
+var dummy9 = {
+	'title' : '\"M, or T?\"',
+	'date' : 'January 2013',
+	'thumbnail_link' : 'img/m_or_t_thumbnail.png',
+	'main_content_link' : '<iframe width="640" height="360" src="https://www.youtube.com/embed/3dblpehTDbg" frameborder="0" allowfullscreen class = "display_img"></iframe>',
+	'description' : '<div class = \"main_content\"><h1>M, or T?</h1><h2>January 2013</h2><p>Created for Information Design and Visualization class while at Penn (Junior Spring).</p><p>It is an exploration of my academic college experience, to determine if I was more "M" (Management) or "T" (Technology). I visualized both my time spent in, and on, Wharton, Engineering, and other classes to better understand how I priotized my academic time.</p><p>Yellow denotes a Wharton course, blue an Engineering one, and green a neutral class (e.g. History, Fine Arts). My schedule each semester is mapped onto the the buildings where the courses were held, with paths indicating my walking path to between the buildings.</p><p>The size of each circle represents the amount of in-class time spent on courses in a specific location. The thickness of the outline of each circle indicates the amount of time I spent on a course outside of class.</p></div>',
+	'categorization' : 'art',
+	'medium' : 'data viz',
+	'content_type' : 'video'
+};
+
+var dummy10 = {
+	'title' : 'Snack time!',
+	'date' : 'March 2010',
+	'thumbnail_link' : 'img/snack_time_thumbnail.png',
+	'main_content_link' : '<img class = "display_img" src ="img/snack_time.png" width = 800px/>',
+	'description' : '<div class = \"main_content\"><h1>Snack time!</h1><h2>March 2010</h2><h2>Colored pencil, 11" x 14"</h2></div>',
+	'categorization' : 'art',
+	'medium' : 'colored pencil',
+	'content_type' : 'video'
+};
+
+var dummy11 = {
+	'title' : 'Peppers',
+	'date' : 'Summer 2010',
+	'thumbnail_link' : 'img/peppers_thumbnail.png',
+	'main_content_link' : '<img class = "display_img" src ="img/peppers.png" width = 800px/>',
+	'description' : '<div class = \"main_content\"><h1>Peppers</h1><h2>Summer 2010</h2><h2>Colored pencil, 18" x 24"</h2></div>',
+	'categorization' : 'art',
+	'medium' : 'colored pencil',
+	'content_type' : 'video'
+};
+
+var markers = {
+	'title' : 'Markers',
+	'date' : 'Spring 2009',
+	'thumbnail_link' : 'img/marker_tryptic_thumbnail.png',
+	'main_content_link' : 'img/marker_tryptic.png',
+	'description' : '<div class = \"main_content\"><h1>Markers</h1><h2>Spring 2009</h2><h2>Acrylic on canvas board, 3 8" x 10" canvases</h2></div>',
+	'categorization' : 'art',
+	'medium' : 'colored pencil',
+	'content_type' : 'image'
+};
+
+var kiln = {
+	'title' : 'Wine Pour',
+	'date' : '2010',
+	'thumbnail_link' : 'img/wine_pour_thumbnail.png',
+	'main_content_link' : '<img class = "display_img" src = "img/wine_pour.png" width = 500px/>',
+	'description' : '<div class = \"main_content\"><h1>Wine Pour</h1><h2>Spring 2010</h2><h2>Colored pencil, 8" x 10"</h2></div>',
+	'categorization' : 'art',
+	'medium' : 'colored pencil',
+	'content_type' : 'video'
+};
+
+var bridges = {
+	'title' : 'Bridges',
+	'date' : 'January 2016',
+	'thumbnail_link' : 'img/bridges_thumbnail.png',
+	'main_content_link' : '<img class = "display_img multi_display" src ="img/bridges.png" width= 800px/>',
+	'description' : '<div class = \"main_content\"><h1>Bridges</h1><h2>January 2016</h2><h2>Acrylic on canvas, 20" x 16"</h2></div>',
+	'categorization' : 'art',
+	'medium' : 'acrylic',
+	'content_type' : 'video'
+};
+
+var abstract_blue = {
+	'title' : 'Untitled',
+	'date' : 'October 2015',
+	'thumbnail_link' : 'img/abstract_blue_thumbnail.png',
+	'main_content_link' : '<img class = "display_img" src ="img/abstract_blue.png" width = 800px/>',
+	'description' : '<div class = \"main_content\"><h1>[Untitled]</h1><h2>October 2015</h2><h2>Oil on canvas, 24" x 18"</h2></div>',
+	'categorization' : 'art',
+	'medium' : 'oil',
+	'content_type' : 'video'
+};
+
+var trees = {
+	'title' : 'A song of purple summer',
+	'date' : 'May 2016',
+	'thumbnail_link' : 'img/trees_thumbnail.png',
+	'main_content_link' : '<a class = "fancybox" data-fancybox-group = "trees" href ="img/trees.png"> <img class = "display_img multi_display" src ="img/trees.png" width = 1000px/></a>',
+	'description' : '<div class = \"main_content\"><h1>A song of purple summer</h1><h2>May 2016</h2><h2>Oil on canvas, 30" x 10"</h2></div>',
+	'categorization' : 'art',
+	'medium' : 'oil',
+	'content_type' : 'video'
+};
+
+var monkeys = {
+	'title' : 'Code Monkeys',
+	'date' : 'December 2012',
+	'thumbnail_link' : 'img/code monkeys_thumbnail.png',
+	'main_content_link' : '<a class = "fancybox" data-fancybox-group = "monkeys" href ="img/code monkeys_1.png"> <img class = "display_img multi_display" src ="img/code monkeys_1.png" width = 482.5px/></a><a class = "fancybox" data-fancybox-group = "monkeys" href ="img/code monkeys_2.png"><img class = "display_img multi_display" src ="img/code monkeys_2.png" width =400px/></a>',
+	'description' : '<div class = \"main_content\"><h1>Code Monkeys</h1><h2>December 2012</h2><p>3D model of literal code monkeys! Created using Autodesk Maya.</p></div>',
+	'categorization' : 'art',
+	'medium' : 'digital',
+	'content_type' : 'video'
+};
+
+var dummy2 = {
+	'title' : 'Physically-based simulation of ice melting',
+	'date' : 'May 2013',
+	'thumbnail_link' : 'img/ice_thumbnail.png',
+	'main_content_link' : '<iframe width="640" height="360" src="https://www.youtube.com/embed/K7hBd012CyU" frameborder="0" allowfullscreen class = "display_img"></iframe>',
+	'description' : '<div class = \"main_content\"><h1>Physically-based simulation of ice melting</h1><h2>Spring 2013</h2><p>A particle based simulation of water phase change, from solid ice to liquid water. The simulation incorporates temperature advection between particles, from ambient temperature, and from other heat sources (objects). It also supports arbitrary ice mesh loading, interfacial forces between the meltwater and ice, and a global gravity force.</p><p>The simulation leverages the open-source fluid particle simulator Fluids V.2, and is based off of the research paper "Fast Particle-based Visual Simulation of Ice Melting."</p></div>',
+	'categorization' : 'firstCODE',
+	'medium' : '',
+	'content_type' : 'video'
+};
+
+var dummy3 = {
+	'title' : 'Voxel renderer',
+	'date' : 'September 2012',
+	'thumbnail_link' : 'img/voxel_1_thumbnail.png',
+	'main_content_link' : '<a class = "fancybox" data-fancybox-group = "voxels" href ="img/voxel_1.png"> <img class = "display_img multi_display" src ="img/voxel_1.png" width = 400px/></a><a class = "fancybox" data-fancybox-group = "voxels" href ="img/voxel_2.png"><img class = "display_img multi_display" src ="img/voxel_2.png" width =400px/></a>',
+	'description' : '<div class = \"main_content\"><h1>Voxel renderer</h1><h2>September 2012</h2><p>A voxel-based volumetric renderer that uses ray casting to determine intersections with the voxels in a scene. A Perlin Noise function was added to create the cloud/smoke effect. Coded in C++.</p></div>',
+	'categorization' : 'code',
+	'medium' : '',
+	'content_type' : 'video'
+};
+
+var smoke = {
+	'title' : 'Semi-Lagrangian Smoke Simulation',
+	'date' : 'March 2013',
+	'thumbnail_link' : 'img/smoke_sim_thumbnail.png',
+	'main_content_link' : '<iframe width="640" height="480" src="https://www.youtube.com/embed/84hFsGp94IU" frameborder="0" allowfullscreen class = "display_img"></iframe>',
+	'description' : '<div class = \"main_content\"><h1>Physically-based simulation of ice melting</h1><h2>UPenn DMD Senior Design Project</h2><h2>Spring 2014</h2><p>A particle based simulation of water phase change, from solid ice to liquid water. The simulation incorporates temperature advection between particles, from ambient temperature, and from other heat sources (objects). It also supports arbitrary ice mesh loading, interfacial forces between the meltwater and ice, and a global gravity force.</p><p>The simulation leverages the open-source fluid particle simulator Fluids V.2, and is based off of the research paper "Fast Particle-based Visual Simulation of Ice Melting."</p></div>',
+	'categorization' : 'code',
+	'medium' : '',
+	'content_type' : 'video'
+};
+
+var projectArray = [];
+projectArray.push(dummy, dummy4, dummy6, dummy7, blocks, dummy8, dummy9, dummy10, dummy11, markers, kiln, bridges, abstract_blue, trees, monkeys, dummy2, dummy3, smoke);
+
+var createList = function(){
+	for(var i = 0; i < projectArray.length; i++){
+		
+		var line = $(document.createElement("div"));
+		line.addClass("line");
+		
+		//create empty elements for each list item
+		var projectListItem = $(document.createElement("div"));
+		var projectListItemThumbnail = $(document.createElement("img"));
+		var projectListItemTitle = $(document.createElement("h2"));
+		var projectListItemDate = $(document.createElement("h3"));
+		var projectListItemLink = $(document.createElement("a"));
+		var projectListItemText = $(document.createElement("div"));
+
+		//populate and structure the list items correctly, pulling from JSO
+		projectListItemLink.attr('href', '#' + i);
+		
+		projectListItem.addClass("project");
+		
+		//IF THE CARD HAS A SPECIAL SIGNIFIER, ADD A LINE
+		if(projectArray[i].categorization === "firstCODE"){
+			$("#project_list_container").append(line);
+		}
+		
+		if(projectArray[i].categorization === "firstART"){
+			$("#project_list_container").append(line);
+		}
+		
+		$("#project_list_container").append(projectListItemLink);
+		projectListItemLink.append(projectListItem);
+		
+		/*projectListItemText.addClass("project_text");
+		
+		projectListItemTitle.text(projectArray[i].title);
+		projectListItemTitle.addClass("title_list");
+		
+		projectListItemDate.text(projectArray[i].date);
+		projectListItemDate.addClass("date_list");
+		*/
+		
+		// if(projectArray[i].medium != ''){
+		// 	projectListItemDate.append(";‬ " + projectArray[i].medium);
+		// }
+		
+		// var categorization = document.createTextNode("; " + projectArray[i].categorization);
+// 		projectListItemDate.append(categorization);
+		
+		projectListItemThumbnail.addClass("image_list");
+		projectListItemThumbnail.attr('src', projectArray[i].thumbnail_link);
+		projectListItemThumbnail.attr('width', '125px');
+		projectListItemThumbnail.attr('height', '125px');
+		
+		projectListItemText.append(projectListItemTitle, projectListItemDate);
+		projectListItem.append(projectListItemThumbnail, projectListItemText);
+		
+		
+		//populate the same item as the main display element, setting all elements (except the first to hidden)
+		var projectDisplay = $(document.createElement("div"));
+		var multiMediaDiv = $(document.createElement("div"));
+		multiMediaDiv.addClass("multimedia_content");
+		if (projectArray[i].content_type == 'image'){
+			var projectContent = $(document.createElement("img"));
+			projectContent.addClass("display_img");
+			projectContent.attr('src', projectArray[i].main_content_link);
+		}
+		
+		if (projectArray[i].content_type == 'video'){
+			var projectContent = $.parseHTML(projectArray[i].main_content_link);
+		}
+		
+		//var projectDisplayDescription = $(document.createElement("p"));
+		//projectDisplayDescription.text(projectArray[i].description);
+		
+		var projectDisplayDescription = $.parseHTML(projectArray[i].description);
+		
+		projectDisplay.addClass("project_display");
+		$("#project_display_container").append(projectDisplay);
+		
+		multiMediaDiv.append(projectContent);
+		
+		projectDisplay.append(multiMediaDiv, projectDisplayDescription);
+		
+		projectDisplay.addClass("item_"+i);
+		projectListItem.addClass("item_"+i);
+		
+		if(i > 0){
+			projectDisplay.addClass("hidden");
+		}
+		else{
+			projectDisplay.addClass("active");
+			projectListItem.addClass("selected");
+		}
+		
+		//add matching classes to the Project List Item and the Project Display, to be able to toggle being active
+		
+	}
+};
+
+var clicker = function(){
+	$('.project').click(function(){
+		var identifyingClass = "." + this.classList[1];
+		$('.project').removeClass("selected");
+		$('.project'+identifyingClass).addClass("selected");
+		$('.project_display').removeClass("active");
+		$('.project_display').addClass("hidden");
+		$(".project_display" + identifyingClass).addClass("active");
+		$(".project_display" + identifyingClass).removeClass("hidden");
+		
+		console.log($('.project').index(this));
+
+	});
+};
+
+function checkInView(listItem){
+	var offset = listItem.offset().top - $("#project_list_container").scrollTop();
+	if(listItem.offset().top < 0){
+	        // Not in view bceause list item is above the browser window so scroll to it
+		$('#project_list_container').animate({scrollTop: 	($("#project_list_container").scrollTop()+listItem.offset().top - 20)}, 250);
+		return false;
+	}
+	
+	if((listItem.offset().top + 50) > $(window).height()){
+		$('#project_list_container').animate({scrollTop: ($("#project_list_container").scrollTop() - ($(window).height() - listItem.offset().top - 100))}, 250);
+		return false;
+	}
+	
+}
+
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+        break;
+
+		case 38: 
+			var previous_main = $(".active").prev();
+			//var previous_list = $(".selected").parent().prev().find(".project");
+			var previous_list = $(".selected").parent().prev();
+			console.log(previous_list);
+
+			if(previous_list.hasClass("line")){
+				previous_list = previous_list.prev();
+			}
+
+			if(!previous_main.hasClass("hidden")){
+			}
+			else{
+				previous_list = previous_list.find(".project");
+				$('.project_display').removeClass("active");
+				$('.project_display').addClass("hidden");
+				previous_main.addClass("active");
+				previous_main.removeClass("hidden");
+				
+				$('.project').removeClass("selected");
+				previous_list.addClass("selected");
+			};
+			checkInView(previous_list);
+        break;
+
+        case 39: // right
+        break;
+
+        case 40:
+			var next_main = $(".active").next();
+			//var next_list = $(".selected").parent().next().find(".project");
+			var next_list = $(".selected").parent().next();
+			console.log(next_list);
+
+			if(next_list.hasClass("line")){
+				console.log("YEE");
+				next_list = next_list.next();
+			}
+
+			if(!next_main.hasClass("hidden")){
+			}
+			else{
+				next_list = next_list.find(".project")
+				$('.project_display').removeClass("active");
+				$('.project_display').addClass("hidden");
+				next_main.addClass("active");
+				next_main.removeClass("hidden");
+				
+				$('.project').removeClass("selected");
+				next_list.addClass("selected");
+				
+				checkInView(next_list);
+
+			};
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
+
+
+
+
+$(document).ready(createList);
+$(document).ready(clicker);
+$(document).ready(function() {
+	$(".fancybox").fancybox();
+	
+$(".fancybox")
+    .attr('rel', 'pixar')
+    .fancybox({
+        padding : 0
+    });
+	
+});
